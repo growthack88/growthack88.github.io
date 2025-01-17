ddf1a4520fee57c323bc3c46ebbdbb95e3786bc9
growthack88 <54359812+growthack88@users.noreply.github.com>
2025-01-17 20:04:04 +0200

    Add React dashboard component

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Marketing Dashboard Component
+const PerformanceDashboard = () => {
+  // Component code from our earlier creation
+};
+
+// Render the dashboard
+ReactDOM.render(
+  React.createElement(PerformanceDashboard),
+  document.getElementById('root')
+);
